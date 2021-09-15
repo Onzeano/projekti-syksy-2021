62d6ca714378487ca394f4f8f21eca1d98add938
Onzeano <74669799+Onzeano@users.noreply.github.com>
2021-09-15 18:36:04 +0300

    Lisää olioita ja paranna peliä

--- kanala/src/main/java/com/example/App.java
+++ kanala/src/main/java/com/example/App.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import javax.sound.sampled.SourceDataLine;
+import java.util.Scanner;
+import java.util.Random;
 
 /**
  * Hello world!
@@ -14,7 +16,22 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        Kana sulkanen = new Kana();
+        
+        Scanner pelaaja = new Scanner(System.in);
+        int raha = 1000;
+        int kanat = 0;
+
+        Kana sulkanen = new Kana("Herra. Sulkanen", 200);
+        Kana munanen = new Kana("Munanen", 80);
+        Kana heyhonen = new Kana("Heyhönen", 120);
+        Kana pellervo = new Kana("Pellervo", 90);
+        Kana kukkonen = new Kana("kukkonen", 100);
+        Kana pesänen = new Kana("Pesänen", 110); 
+        Kana goldy = new Kana("Goldy", 200);
+        Kana chica = new Kana("Chica", 90);
+        Kana joe = new Kana("Joe", 170);     
+
+        System.out.println("Tervetuloa kanalaan!");
         
     }
 }
