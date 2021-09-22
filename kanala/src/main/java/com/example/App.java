2a01148805be0fe739c7074b825444a56a6280b3
Väinö Vahanen <vainovahanen@outlook.com>
2021-09-22 21:02:56 +0300

    Lisää konstruktori olioon ja lisää ehtolause

--- kanala/src/main/java/com/example/App.java
+++ kanala/src/main/java/com/example/App.java
@@ -3,6 +3,7 @@ package com.example;
 import javax.sound.sampled.SourceDataLine;
 import java.util.Scanner;
 import java.util.Random;
+import java.util.ArrayList; 
 
 /**
  * Hello world!
@@ -18,20 +19,48 @@ public final class App {
     public static void main(String[] args) {
         
         Scanner pelaaja = new Scanner(System.in);
+        Random rand = new Random();
+
         int raha = 1000;
         int kanat = 0;
 
         Kana sulkanen = new Kana("Herra. Sulkanen", 200);
-        Kana munanen = new Kana("Munanen", 80);
+        Kana munanen = new Kana("Munanen", 130);
         Kana heyhonen = new Kana("Heyhönen", 120);
         Kana pellervo = new Kana("Pellervo", 90);
         Kana kukkonen = new Kana("kukkonen", 100);
         Kana pesänen = new Kana("Pesänen", 110); 
         Kana goldy = new Kana("Goldy", 200);
-        Kana chica = new Kana("Chica", 90);
-        Kana joe = new Kana("Joe", 170);     
+        Kana kananen = new Kana("Kananen", 90);
+        Kana Siemenen = new Kana("Siemenen", 170);     
 
         System.out.println("Tervetuloa kanalaan!");
+        System.out.println("");
+
+        ArrayList<Kana> kanalista = new ArrayList<Kana>();
+        ArrayList<Kana> kanakauppa = new ArrayList<Kana>();
+        kanakauppa.add(sulkanen);
+        kanakauppa.add(munanen);
+        kanakauppa.add(heyhonen);
+        kanakauppa.add(pellervo);
+        kanakauppa.add(kukkonen);
+        kanakauppa.add(pesänen);
+        kanakauppa.add(goldy);
+        kanakauppa.add(kananen);
+        kanakauppa.add(Siemenen);
         
+        System.out.println("Sinulla on " + raha + " euroa ja " + kanalista.size() + " kanaa." );
+        Kana ostokana = kanakauppa.get(rand.nextInt(kanakauppa.size()));
+        System.out.println("Haluatko ostaa kanan " + ostokana.getNimi() + " kyllä/ei");
+        String vastaus = pelaaja.nextLine();
+        
+        if (vastaus == "kyllä") {
+            
+
+        }
+        
+        //kanakauppa.size()
+        //kanakauppa.get()
+         
     }
 }
--- kanala/src/main/java/com/example/Kana.java
+++ kanala/src/main/java/com/example/Kana.java
@@ -3,11 +3,22 @@ package com.example;
 public class Kana {
     private String nimi;
     private int hinta;
+    
     public Kana() {
         this.nimi = "Herra. Sulkainen";
         this.hinta = 100;
     }
+
+    public Kana(String nimi, int hinta) {
+        this.nimi = nimi;
+        this.hinta = hinta;
+    }
+    
     public String getNimi () {
         return this.nimi;
+    }
+
+    public int getHinta () {
+        return this.hinta;
     }
 }
